99da5a5270b13f2bb8a7b5e70c9d3249b29236e6
Melissa <100771723+ddsmlf@users.noreply.github.com>
2024-05-19 11:48:30 +0200

    [FEAT] Perf

--- utils/utils.py
+++ utils/utils.py
@@ -2,8 +2,10 @@ import os
 
 # Disable OneDNN optimization options
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-
+import pickle
+import tensorflow as tf
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import ResNet50
 from PIL import Image
 from tensorflow.keras.preprocessing.image import img_to_array
 import numpy as np
@@ -18,7 +20,7 @@ def load_images(directory, label):
             if file.lower().endswith(('.png', '.jpg', '.jpeg')):
                 image = Image.open(file_path)
                 image = image.convert('RGB')
-                image = image.resize((64, 64))
+                image = image.resize((224, 224))
                 image = img_to_array(image)
                 images.append(image)
                 labels.append(label)
@@ -32,7 +34,7 @@ def load_images(directory, label):
 def load_image(path):
         image = Image.open(path)
         image = image.convert('RGB')
-        image = image.resize((64, 64))
+        image = image.resize((224, 224))
         image = img_to_array(image)
         image = image / 255.0
         image_input = np.expand_dims(image, axis=0)
@@ -49,3 +51,32 @@ def clear_terminal():
         os.system('cls')
     else:  # Linux and macOS
         os.system('clear')
+
+
+def load_model(model_path = None):
+    """
+    Load a model from a file.
+
+    Parameters:
+        model_path (str): The path to the model file.
+
+    Returns:
+        tensorflow.keras.models.Sequential: The model.
+    """
+    pkl = model_path.split('.h5')[0] + "_info.pkl"
+    with open(pkl, 'rb') as file:
+        info = pickle.load(file)
+    num_classes = info['num_classes']
+    labels = info['labels']
+    base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    model = tf.keras.models.Sequential([
+        base_model,
+        tf.keras.layers.GlobalAveragePooling2D(),
+        tf.keras.layers.Dense(1024, activation='relu'),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+    base_model.trainable = False
+    model.compile(optimizer=Adam(lr=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
+    if model_path is not None:
+        model.load_weights(model_path)
+    return model
